4a2adbbf8440fc897b7abcb22fd9fb0ed7c5bb99
LucasSGonza <lucassilva.goncalves@hotmail.com>
2024-03-01 16:21:27 -0300

    first commit

---
+++ app/src/main/java/com/example/contactlistandroid/model/Contact.kt
@@ -0,0 +1,8 @@
+package com.example.contactlistandroid.model
+
+class Contact(
+    val id: Int,
+    var name: String,
+    var phoneNumber: String
+) {
+}
---
+++ app/src/main/java/com/example/contactlistandroid/modules/main/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.contactlistandroid.modules.main
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity() {
+
+}
